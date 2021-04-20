ecb3f14cf45fa0de7c74b57ba60b9f4aed12faa0
Pit Singert <pisinger@users.noreply.github.com>
2021-04-20 21:23:53 +0200

    spaces replaced

--- Create-PerfCountersViaWinRM/Create-PerfCountersViaWinRM.ps1
+++ Create-PerfCountersViaWinRM/Create-PerfCountersViaWinRM.ps1
@@ -12,17 +12,17 @@
 
 # specify the counters
 Function PerfCounterConfig{
-    New-Item $ConfigPath -type file -Force | Out-Null	
-    # System Performance Counters
-    Add-Content $ConfigPath "`"\Processor Information(*)\% Processor Time`""
-    Add-Content $ConfigPath "`"\Memory\Available Mbytes`""
-    Add-Content $ConfigPath "`"\Network Interface(*)\Output Queue Length`""
-    Add-Content $ConfigPath "`"\Network Interface(*)\Outbound Packets Discarded`""
-    Add-Content $ConfigPath "`"\Network Interface(*)\Inbound Packets Discarded`""
-    Add-Content $ConfigPath "`"\Network Interface(*)\Packets Outbound Discarded`""
-    Add-Content $ConfigPath "`"\Network Interface(*)\Packets Received Discarded`""
-    Add-Content $ConfigPath "`"\PhysicalDisk(*)\Avg. Disk sec/Read`""
-    Add-Content $ConfigPath "`"\PhysicalDisk(*)\Avg. Disk sec/Write`""
+	New-Item $ConfigPath -type file -Force | Out-Null	
+	# System Performance Counters
+	Add-Content $ConfigPath "`"\Processor Information(*)\% Processor Time`""
+	Add-Content $ConfigPath "`"\Memory\Available Mbytes`""
+	Add-Content $ConfigPath "`"\Network Interface(*)\Output Queue Length`""
+	Add-Content $ConfigPath "`"\Network Interface(*)\Outbound Packets Discarded`""
+	Add-Content $ConfigPath "`"\Network Interface(*)\Inbound Packets Discarded`""
+	Add-Content $ConfigPath "`"\Network Interface(*)\Packets Outbound Discarded`""
+	Add-Content $ConfigPath "`"\Network Interface(*)\Packets Received Discarded`""
+	Add-Content $ConfigPath "`"\PhysicalDisk(*)\Avg. Disk sec/Read`""
+	Add-Content $ConfigPath "`"\PhysicalDisk(*)\Avg. Disk sec/Write`""
 }
 
 # call function to create config file on local machine
@@ -35,29 +35,29 @@ $computers = "node01","node02","node03"
 
 foreach ($c in $computers){
 
-    Invoke-Command -ComputerName $c -ScriptBlock {
-        $computer = hostname
-        $computer
+	Invoke-Command -ComputerName $c -ScriptBlock {
+		$computer = hostname
+		$computer
 
-        # save config in temp file
-        $using:config | out-file $using:ConfigPath -Force
+		# save config in temp file
+		$using:config | out-file $using:ConfigPath -Force
 
-        $logmanBin = $env:SystemRoot + "\System32\logman.exe"
-        
+		$logmanBin = $env:SystemRoot + "\System32\logman.exe"
+		
 		# -r = repeat every day
 		# -rf = run for 16h
-        $logmanSyntax = " create counter KHI -o C:\PerfLogs\KHI_$computer -f csv -si 90 -v mmddhhmm -max 50 -b 09/25/2020 06:00:00 -r -rf 16:00:00 -cf $using:ConfigPath"		
-        #$logmanSyntax = " update KHI -b 09/30/2020 06:00:00 -r -rf 16:00:00"
-        #$logmanSyntax = " delete KHI"
-        #$logmanSyntax = " stop KHI"
-        #$logmanSyntax = " start KHI"
+		$logmanSyntax = " create counter KHI -o C:\PerfLogs\KHI_$computer -f csv -si 90 -v mmddhhmm -max 50 -b 09/25/2020 06:00:00 -r -rf 16:00:00 -cf $using:ConfigPath"		
+		#$logmanSyntax = " update KHI -b 09/30/2020 06:00:00 -r -rf 16:00:00"
+		#$logmanSyntax = " delete KHI"
+		#$logmanSyntax = " stop KHI"
+		#$logmanSyntax = " start KHI"
 
-        $cmdLine = $logmanBin + $logmanSyntax
-        Invoke-Expression $cmdLine
+		$cmdLine = $logmanBin + $logmanSyntax
+		Invoke-Expression $cmdLine
 
-        # remove temp config file
-        Remove-Item "$using:ConfigPath" -Force
-    }
+		# remove temp config file
+		Remove-Item "$using:ConfigPath" -Force
+	}
 }
 # remove config from local machine
 Remove-Item "$ConfigPath" -Force
--- Get-TeamsProvInfoFromLocalCache/Get-TeamsProvInfoFromLocalCache.ps1
+++ Get-TeamsProvInfoFromLocalCache/Get-TeamsProvInfoFromLocalCache.ps1
@@ -1,13 +1,13 @@
 # https://github.com/pisinger
 
 <#
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    SOFTWARE.
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
 #>
 
 # requires Powershell 6.0 or above
@@ -22,22 +22,22 @@ $ipaddr = $matches[1]
 $matches = ""
 
 $ProvInfo = [PSCustomObject]@{		
-    Ring 			= $settings.ring
-    Region 			= $settings.region
-    Version			= $settings.version
-    Environment		= $settings.environment
-    TenantRegion 	= $storage.tenantRegion
-    NatIpAddr		= $ipaddr
-    UPN 			= $desktopconfig.upnWindowUserUpn
-    OrgId			= $desktopconfig.userOid
-    TenantId 		= $desktopconfig.userTid
-    GuestTenants	= $desktopconfig.tidOidMap
-    MediaLogging	= $desktopconfig.appPreferenceSettings.enableMediaLoggingPreferenceKey
-    GpuDisabled		= $desktopconfig.appPreferenceSettings.disableGpu
-    Language		= $desktopconfig.currentWebLanguage
-    WebAccountId	= $desktopconfig.webAccountId
-    ElectronVer		= $desktopconfig.lastKnownElectronVersion
-    WebClientVer	= $desktopconfig.previousWebClientVersion
-    UIVersion		= ($skylib | select '*UIVersion' | fl | Out-String).trim()
+	Ring 			= $settings.ring
+	Region 			= $settings.region
+	Version			= $settings.version
+	Environment		= $settings.environment
+	TenantRegion 	= $storage.tenantRegion
+	NatIpAddr		= $ipaddr
+	UPN 			= $desktopconfig.upnWindowUserUpn
+	OrgId			= $desktopconfig.userOid
+	TenantId 		= $desktopconfig.userTid
+	GuestTenants	= $desktopconfig.tidOidMap
+	MediaLogging	= $desktopconfig.appPreferenceSettings.enableMediaLoggingPreferenceKey
+	GpuDisabled		= $desktopconfig.appPreferenceSettings.disableGpu
+	Language		= $desktopconfig.currentWebLanguage
+	WebAccountId	= $desktopconfig.webAccountId
+	ElectronVer		= $desktopconfig.lastKnownElectronVersion
+	WebClientVer	= $desktopconfig.previousWebClientVersion
+	UIVersion		= ($skylib | select '*UIVersion' | fl | Out-String).trim()
 }
 return $ProvInfo
